17188df96f502b1eea0dbe097b7657e06727f359
ulvglzd <ulviguluzada@gmail.com>
2023-11-01 15:31:36 +0400

    extracted out the ErrorObject generator method

--- src/main/java/com/ulviglzd/weatherapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ulviglzd/weatherapi/exceptions/GlobalExceptionHandler.java
@@ -17,96 +17,59 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorObject> handleConstraintViolationException(ConstraintViolationException ex) {
         StringBuilder errorMessages = new StringBuilder();
-
         ex.getConstraintViolations().forEach(constraintViolation -> {
-            errorMessages.append(constraintViolation.getMessage()).append("\n");
+            errorMessages.append(constraintViolation.getMessage() + "\\n");
         });
-
-        ErrorObject errorObject = new ErrorObject().builder()
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .message(errorMessages.toString().trim())
-                .timestamp(LocalDateTime.now())
-                .build();
-
+        ErrorObject errorObject = generateErrorObject(HttpStatus.BAD_REQUEST, errorMessages.toString().trim());
         return ResponseEntity.badRequest().body(errorObject);
     }
 
     @ExceptionHandler(NoSuchCityException.class)
     public ResponseEntity<ErrorObject> handleNoSuchCityException(NoSuchCityException ex) {
-        ErrorObject errorObject = new ErrorObject().builder()
-                .statusCode(HttpStatus.NOT_FOUND.value())
-                .message(ex.getMessage())
-                .timestamp(LocalDateTime.now())
-                .build();
-
+        ErrorObject errorObject = generateErrorObject(HttpStatus.NOT_FOUND, ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorObject);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorObject> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         StringBuilder errorMessages = new StringBuilder();
-
         ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
-            errorMessages.append(fieldError.getDefaultMessage()).append("\n");
+            errorMessages.append(fieldError.getDefaultMessage() + "\\n");
         });
-
-        ErrorObject errorObject = new ErrorObject().builder()
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .message(errorMessages.toString())
-                .timestamp(LocalDateTime.now())
-                .build();
-
-        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
-        errorObject.setMessage(errorMessages.toString());
-        errorObject.setTimestamp(LocalDateTime.now());
-
+        ErrorObject errorObject = generateErrorObject(HttpStatus.BAD_REQUEST, errorMessages.toString().trim());
         return ResponseEntity.badRequest().body(errorObject);
     }
 
     @ExceptionHandler(UserNameAlreadyExistsException.class)
     public ResponseEntity<ErrorObject> handleUserNameAlreadyExistsException(UserNameAlreadyExistsException ex) {
-        ErrorObject errorObject = new ErrorObject().builder()
-                .statusCode(HttpStatus.CONFLICT.value())
-                .message(ex.getMessage())
-                .timestamp(LocalDateTime.now())
-                .build();
-
+        ErrorObject errorObject = generateErrorObject(HttpStatus.CONFLICT, ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorObject);
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<ErrorObject> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
-
-        ErrorObject errorObject = new ErrorObject().builder()
-                .statusCode(HttpStatus.CONFLICT.value())
-                .message(ex.getMessage())
-                .timestamp(LocalDateTime.now())
-                .build();
-
+        ErrorObject errorObject = generateErrorObject(HttpStatus.CONFLICT, ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorObject);
     }
 
     @ExceptionHandler(UnableToUploadFileException.class)
     public ResponseEntity<ErrorObject> handleUnableToUploadFileException(UnableToUploadFileException ex) {
-        ErrorObject errorObject = new ErrorObject().builder()
-                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .message(ex.getMessage())
-                .timestamp(LocalDateTime.now())
-                .build();
-
+        ErrorObject errorObject = generateErrorObject(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorObject);
     }
 
     @ExceptionHandler(UnsupportedFileFormatException.class)
     public ResponseEntity<ErrorObject> handleUnsupportedFileFormatException(UnsupportedFileFormatException ex) {
+        ErrorObject errorObject = generateErrorObject(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorObject);
+    }
 
-        ErrorObject errorObject = new ErrorObject().builder()
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .message(ex.getMessage())
+    private ErrorObject generateErrorObject(HttpStatus status, String message) {
+        return new ErrorObject().builder()
+                .statusCode(status.value())
+                .message(message)
                 .timestamp(LocalDateTime.now())
                 .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorObject);
     }
 
 
